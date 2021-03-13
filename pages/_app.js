dcfa8b17d4cf45706c9880fd8004c6bce16eb6ad
Ali Zahid <ali.zahid@live.com>
2021-03-13 22:46:27 +0500

    Analytics added

--- pages/_app.js
+++ pages/_app.js
@@ -4,12 +4,18 @@ import '../styles/global.scss'
 import { Footer } from '../components/footer'
 import { Header } from '../components/header'
 
-const CryptoWorth = ({ Component, pageProps }) => (
-  <>
-    <Header />
-    <Component {...pageProps} />
-    <Footer />
-  </>
-)
+import { usePanelbear } from '../hooks/bear'
+
+const CryptoWorth = ({ Component, pageProps }) => {
+  usePanelbear('2kbPdGhtw5X')
+
+  return (
+    <>
+      <Header />
+      <Component {...pageProps} />
+      <Footer />
+    </>
+  )
+}
 
 export default CryptoWorth
