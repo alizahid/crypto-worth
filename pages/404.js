ee9a6e6fd84430f454073771c099a077420d82a6
Ali Zahid <ali.zahid@live.com>
2021-03-14 04:55:31 +0500

    Styles updated

--- pages/404.js
+++ pages/404.js
@@ -7,7 +7,7 @@ const NotFound = () => (
       <title>Not found / What would my crypto investment be worth today?</title>
     </Head>
 
-    <main>
+    <main className="flex flex-col justify-center">
       <h2 className="text-6xl font-semibold">Oops!</h2>
       <div className="mt-4 text-xl font-medium">
         We can&#39;t seem to find the page you&#39;re looking for.
