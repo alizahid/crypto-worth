578b47adf57cc5d0fbde45d5c245bf7061b824e6
Ali Zahid <ali.zahid@live.com>
2021-03-14 05:02:43 +0500

    Styles updated

--- components/header.js
+++ components/header.js
@@ -1,39 +1,24 @@
 import Link from 'next/link'
-import { useRouter } from 'next/router'
 import React from 'react'
 
 import { Logo } from './logo'
 
-export const Header = () => {
-  const { asPath } = useRouter()
+export const Header = () => (
+  <header>
+    <nav className="flex items-center">
+      <Link href="/">
+        <a className="text-black font-medium">Calculator</a>
+      </Link>
+      <Link href="/charts">
+        <a className="text-black font-medium ml-4">Charts</a>
+      </Link>
+    </nav>
 
-  return (
-    <header>
-      <nav className="flex items-center">
-        <Link href="/">
-          <a
-            className={`font-medium ${
-              asPath === '/' ? 'text-emerald-700' : 'text-black'
-            }`}>
-            Calculator
-          </a>
-        </Link>
-        <Link href="/charts">
-          <a
-            className={`font-medium ml-4 ${
-              asPath === '/charts' ? 'text-emerald-500' : 'text-black'
-            }`}>
-            Charts
-          </a>
-        </Link>
-      </nav>
-
-      <div className="flex items-center mt-8">
-        <Logo />
-        <h1 className="text-2xl font-semibold flex-1 ml-4">
-          What would my crypto investment be worth today?
-        </h1>
-      </div>
-    </header>
-  )
-}
+    <div className="flex items-center mt-8">
+      <Logo />
+      <h1 className="text-2xl font-semibold flex-1 ml-4">
+        What would my crypto investment be worth today?
+      </h1>
+    </div>
+  </header>
+)
